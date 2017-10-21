9a68bda765189becfd97ed7f037f9d2ad9d4431f
Bluerose69 <32911738+Bluerose69@users.noreply.github.com>
2017-10-21 23:35:32 +0300

    Update Lab4.cpp

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -1,9 +1,17 @@
 ﻿// Lab4.cpp : Defines the entry point for the console application.
 //
 #include "stdafx.h"
+#include <iostream.h>
+#include <conio.h>
+#include <fstream.h>
+const num = 255;
+
+
 
 int main()
-{
-    return 0;
-}
+{ ifstream F;
+  ofstream F1;
+  char s[num] = "";
+  int n = 0;
+  char c;
 
